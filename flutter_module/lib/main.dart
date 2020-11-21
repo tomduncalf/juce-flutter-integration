b3d5f4e6901a9e33e33b27b2ad8498f0c6bed3a7
Tom Duncalf <tom@tomduncalf.com>
2020-11-21 19:02:19 +0000

    plist fun

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -91,7 +91,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many hah times:',
+              'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
